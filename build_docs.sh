b9c0b940c5a52bcae5f5c87a8ea227c62db09102
Garrett Moon <garrett@pinterest.com>
2015-02-15 17:53:38 -0800

    Updating docs

--- build_docs.sh
+++ build_docs.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 appledoc \
-    --company-id com.tumblr \
-    --project-name TMCache \
-    --project-company Tumblr \
-    --project-version 1.2.0 \
+    --company-id com.pinterest \
+    --project-name PINCache \
+    --project-company Pinterest \
+    --project-version 2.0 \
     --docset-min-xcode-version 4.3 \
     --docset-bundle-id %COMPANYID.%PROJECTID \
     --docset-bundle-name "%PROJECT %VERSION" \
@@ -19,5 +19,5 @@ appledoc \
     --output ./docs \
     .
     
-mv docs/docset docs/com.tumblr.TMCache-1.2.0.docset
+mv docs/docset docs/com.pinterest.PINCache-2.0.docset
 rm docs/docset-installed.txt
