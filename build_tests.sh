01a0efe62b405634163d397d8381bc5d5d0e1b68
Garrett Moon <garrett@pinterest.com>
2015-07-28 13:32:02 -0700

    Fix build_tests.sh

--- build_tests.sh
+++ build_tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-xcodebuild ONLY_ACTIVE_ARCH=NO -project tests/TMCache.xcodeproj -scheme TMCacheTests -sdk iphonesimulator TEST_AFTER_BUILD=YES clean build
+xcodebuild ONLY_ACTIVE_ARCH=NO -project tests/PINCache.xcodeproj -scheme PINCacheTests -sdk iphonesimulator TEST_AFTER_BUILD=YES clean build
