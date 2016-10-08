d02fc204323fa6050ffa6280aa88547af2d96d1e
Michael Schneider <mischneider1@gmail.com>
2016-10-08 14:07:07 -0700

    Add cancelAllOperations

--- PINCache/PINOperationQueue.h
+++ PINCache/PINOperationQueue.h
@@ -22,7 +22,17 @@ typedef NS_ENUM(NSUInteger, PINOperationQueuePriority) {
 - (instancetype)initWithMaxConcurrentOperations:(NSUInteger)maxConcurrentOperations;
 - (instancetype)initWithMaxConcurrentOperations:(NSUInteger)maxConcurrentOperations concurrentQueue:(dispatch_queue_t)concurrentQueue NS_DESIGNATED_INITIALIZER;
 - (id <PINOperationReference>)addOperation:(dispatch_block_t)operation withPriority:(PINOperationQueuePriority)priority;
+
+/**
+ * Marks the operation as cancelled
+ */
 - (void)cancelOperation:(id <PINOperationReference>)operationReference;
+
+/**
+ * Cancels all queued operations
+ */
+- (void)cancelAllOperations;
+
 - (void)setOperationPriority:(PINOperationQueuePriority)priority withReference:(id <PINOperationReference>)reference;
 
 @end
