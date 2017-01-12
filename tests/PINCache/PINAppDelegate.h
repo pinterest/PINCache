075d9df19fb02cf4a6d961f0d2b83f74bbb2f3ce
Adlai Holler <guy.on.internet@icloud.com>
2017-01-12 13:00:09 -0800

    Switch to Carthage, rejigger folder structure

--- tests/PINCache/PINAppDelegate.h
+++
@@ -1,9 +0,0 @@
-//  PINCache is a modified version of TMCache
-//  Modifications by Garrett Moon
-//  Copyright (c) 2015 Pinterest. All rights reserved.
-
-@interface PINAppDelegate : UIResponder <UIApplicationDelegate>
-
-@property (strong, nonatomic) UIWindow *window;
-
-@end
--- tests/PINCache/PINExampleView.h
+++
@@ -1,9 +0,0 @@
-//  PINCache is a modified version of TMCache
-//  Modifications by Garrett Moon
-//  Copyright (c) 2015 Pinterest. All rights reserved.
-
-@interface PINExampleView : UIImageView
-
-@property (strong, nonatomic) NSURL *imageURL;
-
-@end
