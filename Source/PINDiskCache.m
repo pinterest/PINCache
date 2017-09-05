492b85272093484e88aac54414583fada9d3ccd6
Justin Patriquin <justin@sidestory.email>
2017-09-05 14:56:57 -0300

    print exception if deserialization fails

--- Source/PINDiskCache.m
+++ Source/PINDiskCache.m
@@ -16,6 +16,10 @@
 [[NSString stringWithUTF8String:__FILE__] lastPathComponent], \
 __LINE__, [error localizedDescription]); }
 
+#define PINDiskCacheException(exception) if (exception) { NSLog(@"%@ (%d) ERROR: %@", \
+[[NSString stringWithUTF8String:__FILE__] lastPathComponent], \
+__LINE__, [exception reason]); }
+
 NSString * const PINDiskCachePrefix = @"com.pinterest.PINDiskCache";
 static NSString * const PINDiskCacheSharedName = @"PINDiskCacheShared";
 
@@ -858,7 +862,7 @@ static NSURL *_sharedTrashURL;
                   [self lock];
                       [[NSFileManager defaultManager] removeItemAtPath:[fileURL path] error:&error];
                   [self unlock];
-                  PINDiskCacheError(error);
+                  PINDiskCacheException(exception);
               }
               [self lock];
             }
