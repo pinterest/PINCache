3a952462c9c8575d3e1677fb6cd4e4e207e459b7
Justin Patriquin <justinpatriquin@gmail.com>
2017-09-18 13:27:40 -0300

    #trivial print exception if deserialization fails (#196)
    * print exception if deserialization fails
    
    * change exception log to an assert, will crash in debug and not production

--- Source/PINDiskCache.m
+++ Source/PINDiskCache.m
@@ -16,6 +16,8 @@
 [[NSString stringWithUTF8String:__FILE__] lastPathComponent], \
 __LINE__, [error localizedDescription]); }
 
+#define PINDiskCacheException(exception) if (exception) { NSAssert(NO, [exception reason]); }
+
 NSString * const PINDiskCachePrefix = @"com.pinterest.PINDiskCache";
 static NSString * const PINDiskCacheSharedName = @"PINDiskCacheShared";
 
@@ -858,7 +860,8 @@ static NSURL *_sharedTrashURL;
                   [self lock];
                       [[NSFileManager defaultManager] removeItemAtPath:[fileURL path] error:&error];
                   [self unlock];
-                  PINDiskCacheError(error);
+                  PINDiskCacheError(error)
+                  PINDiskCacheException(exception);
               }
               [self lock];
             }
