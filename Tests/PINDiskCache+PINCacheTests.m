53d4dad8342ebd38554edf892272093f040cf329
Garrett Moon <garrett@pinterest.com>
2018-06-07 11:49:36 -0700

    Should make tests more reliable

--- Tests/PINDiskCache+PINCacheTests.m
+++ Tests/PINDiskCache+PINCacheTests.m
@@ -1,23 +1,21 @@
 #import "PINDiskCache+PINCacheTests.h"
 
-@interface PINDiskCache ()
-- (void)setTtlCache:(BOOL)ttlCache;
+@interface PINDiskCache () {
+    BOOL _ttlCache;
+}
+
+- (void)lock;
+- (void)unlock;
+
 @end
 
 @implementation PINDiskCache (PINCacheTests)
 
 - (void)setTtlCacheSync:(BOOL)ttlCache
 {
-    [self setTtlCache:ttlCache];
-
-    // Attempt to read from the cache. This will be put on the same queue as `setTtlCache`, but at a lower priority.
-    // When the completion handler runs, we can be sure the property value has been set.
-    dispatch_group_t group = dispatch_group_create();
-    dispatch_group_enter(group);
-    [self objectForKeyAsync:@"some bogus key" completion:^(PINDiskCache *cache, NSString *key, id<NSCoding> object) {
-        dispatch_group_leave(group);
-    }];
-    dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
+    [self lock];
+        self->_ttlCache = ttlCache;
+    [self unlock];
 }
 
 @end
