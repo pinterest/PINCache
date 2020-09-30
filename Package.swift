74b358fe6dc4129c927b15ee424e70d607ed8d9d
3a4oT <petro.rovenskyy@gmail.com>
2020-09-30 13:51:06 +0300

    use PINOperation from - 1.2.0.

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/pinterest/PINOperation.git",  .branch("master")),
+        .package(url: "https://github.com/pinterest/PINOperation.git", from: "1.2.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
